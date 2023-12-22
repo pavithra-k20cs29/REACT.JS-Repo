f77d14cecc12a4f88bd7fbbd8e59b00c58adfa35
pavithra <130351122+pavithra-k20cs29@users.noreply.github.com>
2023-12-22 15:05:17 +0530

    Add files via upload

--- Task1.js
+++ Task1.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React from 'react'
+import style from "./Task1.module.css"
+import Card from 'react-bootstrap/Card';
 
-export function Task1(){
-    return(
-        <>
-        <h1>Hello World</h1>
-        </>
+
+
+const Task1 = () => {
+
+    return (
+        <div className={style.task} >
+            <h1 style={{ textAlign: "center" }}> Congratulations</h1>
+            <div className={style.cards}>
+                <Card style={{ width: '500px', height: '400px', backgroundColor: "aqua" }}>
+                    <Card.Body>
+                        <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png' alt='no images' />
+                        <h3> Kiran </h3>
+                        <h4> Vaishnavi Institution of Technology</h4>
+                        <img src='https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png' alt='no images' />
+                    </Card.Body>
+                </Card>
+            </div>
+        </div>
     )
 }
+
+export default Task1
